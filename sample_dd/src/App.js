43a92d88e4c38d1615ef84d0aa4054d79757aaec
shouyamamoto <yamasyo0017@gmail.com>
2021-03-13 19:09:07 +0900

    move dnd

--- sample_dd/src/App.js
+++ sample_dd/src/App.js
@@ -1,40 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CHARACTERS } from './charactersData'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
-const App = () => {
-  const container = {
-    width: '260px',
-    margin: '0 auto'
-  }
-  const lists = {
-    padding: '40px',
-    margin: '0',
-    border: '1px solid #ccc'
-  }
-  const list = {
-    listStyle: 'none',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItem: 'center',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    padding: '10px',
-    marginBottom: '8px'
+function App() {
+  const [characters, updateCharacters] = useState(CHARACTERS);
+  function handleOnDragEnd(result) {
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
   }
   return (
-      <div className="App" style={container}>
+      <div className="App">
         <header className="App-header">
           <h1>カンバンの簡易版</h1>
-          <DragDropContext>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
           <Droppable droppableId="characters">
           {(provided) => (
-            <ul className="characters" style={lists} {...provided.droppableProps} ref={provided.innerRef}>
-              {CHARACTERS.map(({id, name, thumb}, index) => {
+            <ul 
+              className="characters"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {characters.map(({id, name, thumb}, index) => {
                 return (
                   <Draggable key={id} draggableId={id} index={index}>
                   {(provided) => (
-                    <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={list}>
+                    <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                     <div className="characters-thumb">
                       <img src={thumb} alt={`${name} Thumb`} width="100px" />
                     </div>
